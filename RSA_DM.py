15211c4fec44a7717093cca71676f0482f73dd50
kovski <75985994+kovskitbn@users.noreply.github.com>
2021-04-20 18:08:54 +0200

    Add files via upload

---
+++ RSA_DM.py
@@ -0,0 +1,22 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+liste_lettre = list(alphabet)
+a,b = (3,4)
+u = 0 
+while (a*u)%26!=1:
+        u+=1
+def coder(lettre):
+    y = (a*liste_lettre.index(lettre)+b)%26
+    return (y,liste_lettre[y])
+def decoder(lettre):
+    y = liste_lettre.index(lettre)
+    x = (u*y-u*b)%26
+    return (x,liste_lettre[x])
+mot = 'hdepu'
+mot = list(mot)
+mot_originel = ''
+mot_originel_liste = list()
+for k in mot:
+    mot_originel+=decoder(k)[1]
+    mot_originel_liste.append(decoder(k))
+print(mot_originel, '\n', mot_originel_liste)
+
